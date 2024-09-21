fe54949e0aede435ae6576764b3d66d5d8f4395a
Caroline Cummings <131635586+carolinecummings19@users.noreply.github.com>
2024-09-21 09:40:51 -0400

    logo added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import config from "../../config.json";
 import { Menu, House } from "lucide-react";
 import { withAuthInfo, useRedirectFunctions, useLogoutFunction } from '@propelauth/react'
+import logo from "../assets/chilla_logo.png";
 
 const NavBar = withAuthInfo((props) => {
   const navigate = useNavigate();
@@ -24,54 +25,54 @@ const NavBar = withAuthInfo((props) => {
     navigate("/");
   };
 
-  return (
+return (
     <div
-      className={`fixed top-0 left-0 w-full bg-[--champagne] text-[--black] border-b-[3px] border-gray h-16 flex flex-row items-center justify-between`}
+        className={`fixed top-0 left-0 w-full bg-[--champagne] text-[--black] border-b-[3px] border-gray h-24 flex flex-row items-center justify-between`}
     >
-      <div className="flex items-center">
-        <div onClick={home} className="px-4 cursor-pointer">
-          <Menu size={30} />
+        <div className="flex items-center">
+            <div onClick={home} className="px-4 cursor-pointer">
+                <img src={logo} alt="Chilladelphia Logo" className="h-20 w-20" />
+            </div>
+            <div className="font-bold text-[24pt] mx-1 font-[Libre-Baskerville]" onClick={home}>
+                    Chilladelphia
+            </div>
         </div>
-        <div className="font-bold text-xl mx-4" onClick={home}>
-            Chilladelphia
+            {/* Right side - Buttons */}
+        <div className="flex items-center space-x-4">
+            {props.isLoggedIn ? (
+                <>
+                    <button
+                        onClick={() => redirectToAccountPage()}
+                        className="px-4 py-2 bg-[--highlight] text-black rounded-md"
+                    >
+                        Account
+                    </button>
+                    <button
+                        onClick={() => logoutFunction(true)}
+                        className="px-4 py-2 bg-[--highlight] text-black rounded-md"
+                    >
+                        Logout
+                    </button>
+                </>
+            ) : (
+                <>
+                    <button
+                        onClick={() => redirectToLoginPage()}
+                        className="px-4 py-2 bg-[--highlight] text-black rounded-md"
+                    >
+                        Login
+                    </button>
+                    <button
+                        onClick={() => redirectToSignupPage()}
+                        className="px-4 py-2 bg-[--highlight] text-black rounded-md"
+                    >
+                        Signup
+                    </button>
+                </>
+            )}
         </div>
-      </div>
-        {/* Right side - Buttons */}
-      <div className="flex items-center space-x-4">
-        {props.isLoggedIn ? (
-          <>
-            <button
-              onClick={() => redirectToAccountPage()}
-              className="px-4 py-2 bg-[--highlight] text-black rounded-md"
-            >
-              Account
-            </button>
-            <button
-              onClick={() => logoutFunction(true)}
-              className="px-4 py-2 bg-[--highlight] text-black rounded-md"
-            >
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <button
-              onClick={() => redirectToLoginPage()}
-              className="px-4 py-2 bg-[--highlight] text-black rounded-md"
-            >
-              Login
-            </button>
-            <button
-              onClick={() => redirectToSignupPage()}
-              className="px-4 py-2 bg-[--highlight] text-black rounded-md"
-            >
-              Signup
-            </button>
-          </>
-        )}
-      </div>
-      </div>
-  );
+        </div>
+);
 });
 
 export default NavBar;
