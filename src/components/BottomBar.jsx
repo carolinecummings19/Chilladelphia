6b845b627524c13b1891b353fcc79223df4f5f0d
Caroline Cummings <131635586+carolinecummings19@users.noreply.github.com>
2024-09-22 11:16:06 -0400

    polar bear added

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import polar from '../assets/polar_bear.png';
 
 const BottomBar = () => {
   const currentYear = new Date().getFullYear();
@@ -6,6 +7,7 @@ const BottomBar = () => {
   return (
     <footer className="bottom-0 w-full bg-gray-100 text-center py-2 border-t border-gray-300">
       <div className="flex flex-row items-center justify-center max-w-7xl mx-auto px-4">
+        <img src={polar} alt="polar bear" className="h-10 ml-4 p-1" />
         <p className="text-gray-700 font-bold">&copy; {currentYear} Chilladelphia. All rights reserved.</p>
         <div className="border-l border-gray-500 h-6 mx-4"></div>
         <div>
