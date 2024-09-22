9884222b921d51e6e6ca938e9a4d827547f8688b
Caroline Cummings <131635586+carolinecummings19@users.noreply.github.com>
2024-09-22 08:16:59 -0400

    home page changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,102 @@
 /** @format */
 
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import axios from "axios";
 import config from "../../config.json";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar.jsx";
 import BottomBar from "../components/BottomBar.jsx";
-import heatMap from "../assets/heat_map_home.png";
-
+      
 export default function Home() {
   const rootURL = config.serverRootURL;
 
   const navigate = useNavigate();
 
-//   const fetchData = async () => {
-//     // fetch map and set appropriate state variables
-//     try {
-//       const responseMap = await axios.get(`${rootURL}/${username}/map`);
-//       setPosts(responseMap.data.results);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
-//   };
+  const phillyLocations = [
+    { attraction: "Independence Hall", coordinates: { lat: 39.9519552, long: -75.1946941 } },
+    { attraction: "Liberty Bell", coordinates: { lat: 39.949610, long: -75.150282 } },
+    { attraction: "Philadelphia Museum of Art", coordinates: { lat: 39.965606, long: -75.180246 } },
+    { attraction: "Reading Terminal Market", coordinates: { lat: 39.953186, long: -75.159540 } },
+    { attraction: "Eastern State Penitentiary", coordinates: { lat: 39.968446, long: -75.172022 } },
+    { attraction: "The Franklin Institute", coordinates: { lat: 39.958356, long: -75.173749 } },
+    { attraction: "Philadelphia Zoo", coordinates: { lat: 39.973799, long: -75.195737 } },
+    { attraction: "Penn's Landing", coordinates: { lat: 39.944907, long: -75.141612 } },
+    { attraction: "Rittenhouse Square", coordinates: { lat: 39.948878, long: -75.171467 } },
+    { attraction: "South Street", coordinates: { lat: 39.941028, long: -75.151212 } },
+  ];
+  
+  const randomLocation = phillyLocations[0];
 
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
+  const [analyzedImage, setAnalyzedImage] = useState(null);
+  const [originalImage, setOriginalImage] = useState(null);
+  const [location] = useState({
+  lat: randomLocation.coordinates.lat,
+  long: randomLocation.coordinates.long,
+  });
+
+  useEffect(() => {
+    console.log("Location:", location);
+        if (location) {
+            console.log("Location coordinates:", location.lat, location.long);
+    
+            axios.get(`${rootURL}/getImage`, {
+                params: {
+                    lat: location.lat,
+                    long: location.long
+                }
+            })
+            .then((response) => {
+                console.log("Image URL response:", response.data);
+                setAnalyzedImage(response.data.analyzed_image);
+                setOriginalImage(response.data.original_image)
+            })
+            .catch((error) => {
+                console.error("Error fetching image URL:", error);
+            });
+        } else {
+            console.log("No addresses found for the inputted address.");
+        }
+  }, [location, rootURL]);
 
   return (
-    <div className="flex flex-col">
-      <NavBar />
-      <div className="w-screen h-full bg-[--light-taupe-grey] overflow flex justify-center">
-        <div className="w-screen h-full flex flex-col items-center my-10 text-lg font-Lato space-y-4">
-          <h1 className="font-bold font-[Libre-Baskerville]">Welcome to Chilladelphia!</h1>
-          <p>Here you can find the hottest spots in Philly and how to make the city cooler.</p>
-          <p>Check out our map to find urban heat spots!</p>
-          <button onClick={() => navigate("/map")} className="px-6 py-3 rounded-md bg-[--cambridge-blue] hover:bg-[--khaki] outline-none w-1/5 font-bold text-white font-Lato">
-            Explore the Map
-          </button>
-          <img src={heatMap} alt="Heat Map Image" className="w-3/4 p-2" />
+  <div className="flex flex-col">
+    <NavBar />
+    <div className="w-screen h-full bg-[--light-taupe-grey] overflow flex justify-center">
+    <div className="w-screen h-full flex flex-col items-center my-10 text-lg font-Lato space-y-2">
+      <h1 className="font-bold font-[Libre-Baskerville]">Chilladelphia</h1>
+      <p>Here you can find the "coolest" spots in Philly with lots of tree coverage and how to make the city cooler.</p>
+      <p>Check out our map to find urban heat spots!</p>
+      <button onClick={() => navigate("/map")} className="px-6 py-3 rounded-md bg-[--cambridge-blue] hover:bg-[--khaki] outline-none w-1/5 font-bold text-white font-Lato">
+      Explore the Map
+      </button>
+      <h2 className="font-bold font-Lato text-2xl pt-5">Featured Location: {randomLocation.attraction}</h2>
+      <div className="max-w-3xl mx-auto px-4">
+        <div className="flex flex-row space-x-4 mt-4"> 
+          {originalImage && (
+          <div className="mt-4">
+            <h3 className="text-lg font-semibold mb-2">Original Image</h3>
+            <img 
+            src={`data:image/png;base64,${originalImage}`} 
+            alt="Original Map" 
+            className="w-full h-auto rounded-md" 
+            />
+          </div>
+          )}
+          {analyzedImage && (
+          <div className="mt-4">
+            <h3 className="text-lg font-semibold mb-2">Analyzed Image</h3>
+            <img 
+            src={`data:image/png;base64,${analyzedImage}`} 
+            alt="Analyzed Map" 
+            className="w-full h-auto rounded-md" 
+            />
+          </div>
+          )}
         </div>
       </div>
-      <BottomBar />
     </div>
+    </div>
+    <BottomBar />
+  </div>
   );
 }
